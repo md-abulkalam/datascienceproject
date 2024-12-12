230e265cfc3f00a197baec1807a977d1d35906cb
md-abulkalam <mdabulkalamazad15aug@gmail.com>
2024-12-12 20:21:37 +0600

    model evaluation completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.datascienceproject.pipeline.data_ingestion_pipeline import DataIngestio
 from src.datascienceproject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascienceproject.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascienceproject.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
-
+from src.datascienceproject.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,8 +19,8 @@ except Exception as e:
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataValidationTrainingPipeline()
-   data_ingestion.initiate_data_validation()
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
@@ -30,8 +30,8 @@ except Exception as e:
 STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataTransformationTrainingPipeline()
-   data_ingestion.initiate_data_transformation()
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
@@ -41,13 +41,22 @@ except Exception as e:
 STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = ModelTrainerTrainingPipeline()
-   data_ingestion.initiate_model_training()
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.initiate_model_training()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
--- src/datascienceproject/config/configuration.py
+++ src/datascienceproject/config/configuration.py
@@ -5,6 +5,7 @@ from src.datascienceproject.entity.config_entity import DataIngestionConfig
 from src.datascienceproject.entity.config_entity import DataValidationConfig
 from src.datascienceproject.entity.config_entity import DataTransformationConfig
 from src.datascienceproject.entity.config_entity import ModelTrainerConfig
+from src.datascienceproject.entity.config_entity import ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -75,4 +76,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/akazad/datascienceproject.mlflow"
+
+
+        )
+        return model_evaluation_config
     
